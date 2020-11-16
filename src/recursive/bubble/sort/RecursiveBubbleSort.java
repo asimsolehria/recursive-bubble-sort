fbf96491d57ee1ec4efcd767b9b4e1bf57c29852
Asim Habib <de.asimhabib@gmail.com>
2020-11-16 23:42:40 +0500

    Made changes and wrote half recursive bubble sort

--- src/recursive/bubble/sort/RecursiveBubbleSort.java
+++ src/recursive/bubble/sort/RecursiveBubbleSort.java
@@ -24,7 +24,24 @@ public class RecursiveBubbleSort {
     public void bubbleSort(int arr[],int n, int i)
     {
         bubble(arr, n);
-        if(i<n)
+        if(n>i)
             bubbleSort(arr, n, i+1);
+    }
+    
+    
+    public void halfRecursiveBubble(int arr[],int n)
+    {
+        for (int i = 0; i < arr.length-1; i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+        
+        if(n>0)
+            halfRecursiveBubble(arr, n-1);
     }
 }
--- src/recursive/bubble/sort/main.java
+++ src/recursive/bubble/sort/main.java
@@ -8,6 +8,13 @@ package recursive.bubble.sort;
 public class main {
     
     public static void main(String[] args) {
+        
+//        
+//        obj.halfRecursiveBubble(arr, arr.length-1);
+//        
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//        }
         int arr[]={9,8,7,6,5,4,3,2,1};
         
         RecursiveBubbleSort obj=new RecursiveBubbleSort();
@@ -17,5 +24,6 @@ public class main {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+
     }
 }
